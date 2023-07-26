26e2f5a8e4c5fe4be9cde20da415761bdea2aa48
Nawrssama <nawrsbibi.nb@gmail.com>
2023-07-26 07:49:21 +0000

    resolved lab-38

--- cookie-stand-admin/components/Footer.js
+++ cookie-stand-admin/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <>
-      <footer className="flex items-center pl-20 bg-green-500 h-24">
+      <footer className="flex align-bottom items-center pl-20 bg-green-500 h-24">
         <h1 className="flex text-3xl justify-center "> 8 Locations World Wide </h1>
       </footer>
       </>
--- cookie-stand-admin/components/ReportTable.js
+++ cookie-stand-admin/components/ReportTable.js
@@ -1,24 +1,24 @@
 import { hourly_sales } from "../data";
 import { locations } from "../data";
-import { totals } from "../data";
+// import { totals } from "../data";
 
-export default function ReportTable() {
+export default function ReportTable({formData}) {
     return (
         <div className="flex items-center justify-center m-8">
             <table className='text-center'>
                 <thead>
                     <tr>
                         {hourly_sales.map((item , index) => (
-                            <th key={index} className='p-4 bg-green-600'>{item}</th>
+                            <th key={item} className='p-4 bg-green-600'>{item}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-
-                    {locations.map((item , index) => (
-                        <tr key={index} className={index % 2 != 0 ? 'bg-green-300' : 'bg-green-400'}>
-                            {item.map((element , index) => (
-                                <td key={index} className='border border-black '>{element}</td>
+                    {formData.map((item,indx)=>(
+                        <tr key={indx} className={indx % 2 != 0 ? 'bg-green-300' : 'bg-green-400'}>
+                            <td className='border border-black '>{item.location}</td>
+                            {locations.map((item,indx)=>(
+                                <td key={item} className='border border-black '>{item}</td>
                             ))}
                         </tr>
                     ))}
@@ -26,8 +26,9 @@ export default function ReportTable() {
                 </tbody>
                 <tfoot>
                     <tr>
-                        {totals.map((item , index) => (
-                            <td key={index} className='p-4 font-bold bg-green-600 border border-black' >{item}</td>
+                        <td className='p-4 font-bold bg-green-600 border border-black'>Totals</td>
+                        {locations.map((item , index) => (
+                            <td key={index} className='p-4 font-bold bg-green-600 border border-black' >{item*formData.length}</td>
                         ))}
                     </tr>
                 </tfoot>
